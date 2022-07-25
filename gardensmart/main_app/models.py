073ea8266403a3614f2b6a35db83ff720b44ad06
Paul Yeung <paul.r.yeung@gmail.com>
2022-07-25 14:09:31 -0400

    add user schema

--- gardensmart/main_app/models.py
+++ gardensmart/main_app/models.py
@@ -8,3 +8,21 @@ class Veg(models.Model):
     cost = models.FloatField(default = 0.0)
     seeded = models.IntegerField(default = 0)
     stage = models.CharField(max_length = 50)
+
+
+class Input(models.Model):
+    name = models.CharField(max_length = 50)
+    category = models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
+    cost = models.FloatField(default = 0.0)
+
+    veg = models.ManyToManyField(Veg)
+
+    #add user model later
+
+    def __str__(self):
+        return self.NAME
+
+    #add get absolute url later
+
+
