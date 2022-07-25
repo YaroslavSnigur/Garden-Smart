e1e2524b197e4bb23ff77e63cccc2cff48e70e69
Paul Yeung <paul.r.yeung@gmail.com>
2022-07-25 17:16:49 -0400

    details html barebones

--- gardensmart/main_app/urls.py
+++ gardensmart/main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns= [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('veggies/', views.veggies_index, name='index'),
-    path('accounts/signup/', views.signup, name='signup')
+    path('accounts/signup/', views.signup, name='signup'),
+    path('veggies/', views.veggies_index, name='index'),    
+    path('veggies/<int:veg_id>', views.veg_detail, name='detail'),
 ]
