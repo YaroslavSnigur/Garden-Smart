34f35868c145432f922e670f9499ec6ec03a396e
Paul Yeung <paul.r.yeung@gmail.com>
2022-07-25 15:06:12 -0400

    create user schema and login logic

--- gardensmart/main_app/views.py
+++ gardensmart/main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Veg, Input
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 # Create your views here.
 
@@ -13,3 +14,13 @@ def home(request):
 def about(request):
     return HttpResponse('Insert about.html page here one day')
     #return render(request, 'about.html')
+
+
+
+class VegCreate(CreateView):
+    model = Veg
+    fields = [ 'name', 'description', 'cost', 'date', 'planted', 'stage' ]
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
