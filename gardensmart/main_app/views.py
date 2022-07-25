09791f20f07eb670d4805c2d16790d34c0d73f51
Paul Yeung <paul.r.yeung@gmail.com>
2022-07-25 11:32:20 -0400

    made home and about path, created templates folder

--- gardensmart/main_app/views.py
+++ gardensmart/main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+
+def home(request):
+    return HttpResponse('Hello Yaro and Jason')
+    #return render(request, 'home.html')
+
+def about(request):
+    return HttpResponse('Insert about.html page here one day')
+    #return render(request, 'about.html')
