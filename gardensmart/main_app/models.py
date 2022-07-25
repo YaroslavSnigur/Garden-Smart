8a99594307a897d45d77f77d520686b5f490ad5a
Yaroslav Snigur <yaroslavsnigur@Yaroslavs-MacBook-Air.local>
2022-07-25 12:49:37 -0400

    Veg Schema added

--- gardensmart/main_app/models.py
+++ gardensmart/main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Veg(models.Model):
+    name = models.CharField(max_length = 50)
+    description = models.TextField(max_length = 250)
+    cost = models.FloatField(default = 0.0)
+    seeded = models.IntegerField(default = 0)
+    stage = models.CharField(max_length = 50)
