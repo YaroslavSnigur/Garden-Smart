175163dea603a85232d51dcb9268501dcfcebe5c
Paul Yeung <paul.r.yeung@gmail.com>
2022-07-26 16:12:56 -0400

    added Profile to admin.py

--- gardensmart/main_app/admin.py
+++ gardensmart/main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Veg, Input
+from .models import Veg, Input, Profile
 
 # Register your models here.
 admin.site.register(Veg)
 admin.site.register(Input)
+admin.site.register(Profile)
